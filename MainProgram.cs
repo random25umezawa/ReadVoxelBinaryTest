92d17700d05da9778aa8da45d7ee0f3d9c46c9b0
Umezawa Toshifumi <bday7125@gmail.com>
2017-12-17 01:10:52 +0900

    全点認識できたかのテスト。

--- MainProgram.cs
+++ MainProgram.cs
@@ -5,8 +5,16 @@ class MainProgram
 {
 	static void Main()
 	{
-		VoxData vd = new VoxData(new BinaryReader("res/kusa.vox"));
+		VoxData vd = new VoxData(new BinaryReader("res/3x3x3.vox"));
 		Console.Write(vd.Type);
-		Console.Write(vd.Version);
+		Console.WriteLine(vd.Version);
+		Console.WriteLine(vd.NumModels);
+		foreach(int[,] voxels in vd.Xyzas)
+		{
+			Console.WriteLine(voxels.Length);
+			for(int i = 0; i < voxels.Length/4; i++) {
+				Console.WriteLine("({0,3:G},{1,3:G},{2,3:G},{3,3:G})",voxels[i,0],voxels[i,1],voxels[i,2],voxels[i,3]);
+			}
+		}
 	}
 }
