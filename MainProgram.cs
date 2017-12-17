96a800cc7fefccc0563a1d73d0d0dbd3e2dde24d
Umezawa Toshifumi <bday7125@gmail.com>
2017-12-17 23:49:38 +0900

    Bitmap処理にした上で、回転処理の下地を描画。

--- MainProgram.cs
+++ MainProgram.cs
@@ -6,33 +6,28 @@ class MainProgram
 {
 	static void Main()
 	{
-		VoxData vd = new VoxData(new BinaryReader("res/3x3x3.vox"));
+		VoxData vd = new VoxData(new BinaryReader("res/kusa.vox"));
 		Console.Write(vd.Type);
 		Console.WriteLine(vd.Version);
 		Console.WriteLine(vd.NumModels);
-		
+
 		int[,] sizes = vd.Sizes;
-		int[][,,] danmens = vd.Danmens;
-		for(int model_index = 0; model_index < vd.NumModels; model_index++)
+		Bitmap[] danmens = vd.Danmens;
+		Bitmap[][] kaitentai = vd.Kaitentai;
+		//foreach(Bitmap img in danmens)
+		for(int modelIndex = 0; modelIndex < danmens.Length; modelIndex++)
 		{
-			Bitmap img = new Bitmap(sizes[model_index,0]*sizes[model_index,2],sizes[model_index,1]);
-			img.MakeTransparent();
-			for(int i = 0; i < sizes[model_index,0]*sizes[model_index,2]; i++)
-			{
-				for(int j = 0; j < sizes[model_index,1]; j++)
-				{
-					Color c = Color.FromArgb
-					(
-						danmens[model_index][i,j,3],
-						danmens[model_index][i,j,0],
-						danmens[model_index][i,j,1],
-						danmens[model_index][i,j,2]
-					);
-					img.SetPixel(i,j,c);
-				}
-			}
-			img.Save("result/"+model_index+".png",System.Drawing.Imaging.ImageFormat.Png);
+			Bitmap img = danmens[modelIndex];
+			//img.MakeTransparent();
+			img.Save("result/"+modelIndex+".png",System.Drawing.Imaging.ImageFormat.Png);
 			img.Dispose();
+
+			for(int angleCount = 0; angleCount < kaitentai[modelIndex].Length; angleCount++)
+			{
+				img = kaitentai[modelIndex][angleCount];
+				img.Save("result/"+modelIndex+"_"+angleCount+".png",System.Drawing.Imaging.ImageFormat.Png);
+				img.Dispose();
+			}
 		}
 	}
 }
